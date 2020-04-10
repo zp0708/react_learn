406b02bce5f8bf16bc28881452c5d133a35966f8
bimuyu <bimuyu@helijia.com>
2020-04-10 19:13:49 +0800

    init

---
+++ src/Item.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Item extends Component {
+  shouldCompnentMount() {
+
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
+
+  render() {
+    return (
+      <div>ttttt</div>
+    )
+  }
+}
+
+export default Item
